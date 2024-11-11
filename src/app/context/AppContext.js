8659279bb264047070a2e5a37a8fca07a24b1d92
Abdelrahman mohamed <abdelrahmanelnobby@gmail.com>
2024-11-11 19:56:13 +0200

    updates

--- src/app/context/AppContext.js
+++ src/app/context/AppContext.js
@@ -27,7 +27,7 @@ export const AppProvider = ({ children }) => {
   const fetchFreelancers = async () => {
     try {
       const response = await axios.get(`${process.env.BASE_URL}/users/freelancers`);
-      const sortedFreelancers = response.data.users.sort((a, b) => 
+      const sortedFreelancers = response.data.freelancers.sort((a, b) => 
         new Date(b.createdAt) - new Date(a.createdAt)
       );
       setFreelancers(sortedFreelancers);
